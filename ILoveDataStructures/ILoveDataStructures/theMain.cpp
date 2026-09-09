7f8c45050b3e91e9a89c774ed5a1b312f84f54a8
michaelfeeney <michaelwcfeeney@gmail.com>
2026-09-09 18:59:49 -0400

    End of class code

--- ILoveDataStructures/ILoveDataStructures/theMain.cpp
+++ ILoveDataStructures/ILoveDataStructures/theMain.cpp
@@ -6,8 +6,65 @@
 
 #include <iostream>	// cout << "hi";    cin >> a; 
 
-int main()
+class cPerson
 {
+	int age;
+	float height;
+};
+
+int main(int argc, char* argv[])
+{
+	cPerson Bob;					// STACK
+	cPerson* pBob = new cPerson();	// HEAP
+
+
+	//int t1 = 3;			// 1002
+	//int t2 = 5;			// 1003
+	//int t3 = 71;			// 1004
+	////....
+	//int t100 = 92;
+
+	//int tArray[100];
+
+	int tArray[5] = { 8, 27, 288, 234, 17 };
+	int sizeoftArray = 5;
+	// tArray[0] = 1
+	// tArray[1] = 27
+
+	for (int index = 0; index < 5; index++)
+	{
+		std::cout << index << " : " << tArray[index] << std::endl;
+	}
+	std::cout << "----------------" << std::endl;
+
+	// Pointing to the start of the array
+	int* pArray = tArray;	// int* pArray = &( tArray[0] );
+	
+	//int theVal = tArray[1];
+	int* pArray2 = &(tArray[0]);
+	std::cout << *pArray2 << std::endl;
+
+	// int tArray[5] = { 8, 27, 288, 234, 17 };
+	std::cout << "--------------------" << std::endl;
+	std::cout << " fun with pointers!! " << std::endl;
+	int* pArrayLoc = tArray;	//  of int* pArrayLoc = &(tArray[0])
+	for (int index = 0; index < 10; index++)
+	{
+		std::cout << *pArrayLoc << std::endl;
+		pArrayLoc++;	// Move to next array location
+	}
+	std::cout << "--------------------" << std::endl;
+
+	//int* t; 
+	std::cout << tArray[0] << std::endl;
+	std::cout << tArray << std::endl;
+	std::cout << &( tArray[0] ) << std::endl;
+	std::cout << sizeof(tArray) << std::endl;
+	std::cout << "----------------" << std::endl;
+
+
+
+
 	int a = 333; 
 	int b = 273;
 
@@ -33,32 +90,31 @@ int main()
 
 	//a = 61;
 
-
 	int c = a + b;
 
 	std::cout << "c = " << c << std::endl;
 
-	doThis(a);  // 62
+//	doThis(a);  // 62
 	std::cout << a;
 
-	int& b = a; 
+//	int& b = a; 
 	return 0;
 }
 
-void doThis(int c)
-{
-	c++;
-	std::cout << c << std::endl;
-}
-
-void doThis(int &c)
-{
-	c++;
-	std::cout << c << std::endl;
-}
-
-void doThis(int* p_c)
-{
-	*p_c++;	// (*p_c)++
-	std::cout << *p_c << std::endl;
-}
+//void doThis(int c)
+//{
+//	c++;
+//	std::cout << c << std::endl;
+//}
+//
+//void doThis(int &c)
+//{
+//	c++;
+//	std::cout << c << std::endl;
+//}
+//
+//void doThis(int* p_c)
+//{
+//	*p_c++;	// (*p_c)++
+//	std::cout << *p_c << std::endl;
+//}
